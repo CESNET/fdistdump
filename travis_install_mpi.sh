f80b4f143a816f0920fd2d429810ceb1a9f5b0e8
Jan Wrona <xwrona00@stud.fit.vutbr.cz>
2015-08-06 15:40:16 +0200

    travis: modify MPI installation script, add compiler version print

--- travis_install_mpi.sh
+++ travis_install_mpi.sh
@@ -9,11 +9,11 @@ fi
 
 case $1 in
         mpich)
-                echo sudo apt-get install -q mpich;;
+                sudo apt-get install -q mpich;;
         openmpi)
                 #default OMPI version in Ubuntu 12.10 LTS (1.4) doesn't comply
                 #MPI 2.2, we need at least OMPI 1.5
-                echo sudo apt-get install -q openmpi1.5-bin libopenmpi1.5-dev;;
+                sudo apt-get install -q openmpi1.5-bin libopenmpi1.5-dev;;
         *)
                 echo "Error: unknown MPI implementation:" $1;
                 exit 1;;
